599d6b9c1c566f1fdc6079e8fe10e649acdc923b
root <git@fschneider.at>
2020-10-25 00:14:25 +0200

    add redis support

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,20 @@
 FROM ubuntu
 
 RUN apt-get update
-RUN apt-get install -y curl openssh-server ca-certificates 2>&1 > /dev/null
+RUN apt-get install -y          \
+				curl            \	
+				openssh-server  \
+				ca-certificates \
+				netcat          \
+				less            \
+			2>&1 > /dev/null
 
 # DISABLE TO USE EXTERNAL MTA
 RUN apt-get install -y postfix 2>&1 > /dev/null
 
 RUN curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | bash
+
+RUN apt install -y gitlab-ce
 
 ADD ./entrypoint.sh /entrypoint.sh
                                   
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# wait for redis server
+if [ ! -z "REDIS_SERVER" ]; then
+	# configure redis server
+	sed "s/.*gitlab_rails\['redis_host'\].*/gitlab_rails['redis_host'] = 'redis'/g" /etc/gitlab/gitlab.rb | grep redis_host
+	
+	echo "wait for redis server..."
+	while ! nc -z redis 6379; do
+		echo "wait for redis server..."
+		sleep 5
+	done
+fi
+
+# configure hostname
+grep -Eq "^external_url .*$HOSTNAME" /etc/gitlab/gitlab.rb ||
+	sed -i "s/.*external_url.*/external_url  \"${HOSTNAME}\"/g" /etc/gitlab/gitlab.rb &&
+	gitlab-ctl reconfigure
+
+
 while true; do
 	echo start
 	sleep 1000;
